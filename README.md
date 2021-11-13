09a3c56dd292a3b93ab7be0481550350407d0ea6
Meshach Adoe <madoe@ucsd.edu>
2021-11-12 21:15:46 -0800

    Complete explore section

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ The automated tests should be configured within a Github action that runs whenev
 *2. Would you use an end to end test to check if a function is returning the correct output? (yes/no)*
 
 No, to check the behavior of a specific function, it would be more appropriate to use a unit test. An end to end test involves emulating user actions, hence tests the behavior of the application in response to user input.
+
+*3. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.*
+No, it would not be appropriate to use a unit test for such a use case since that feature would deal with emulating user input to simulate the conversation. The fact that it deals with input from the user makes it more appropriate to use end to end tests.
+
+*4. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.*
+Yes, this would be appropriate since it would only pertain to the behavior of a single function, and is not necessarily dependent on the type of input that the user passes in. The unit test would pass in a predefined input and expect the function to return a particular output.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,108 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+// isPhoneNumber() Unit Tests
+test('expects 858-123-3455 to be a valid phone number', () => {
+    const phoneNumber = '858-123-3455';
+    expect(functions.isPhoneNumber(phoneNumber)).toBe(true);
+})
+
+test('expects 023-413-0919 to be a valid phone number', () => {
+    const phoneNumber = '023-413-0919';
+    expect(functions.isPhoneNumber(phoneNumber)).toBe(true);
+})
+
+test('expects 1234567890 to be an invalid phone number', () => {
+    const phoneNumber = '1234567890';
+    expect(functions.isPhoneNumber(phoneNumber)).toBe(false);
+})
+
+test('expects adc-413-da12 to be an invalid phone number', () => {
+    const phoneNumber = 'adc-413-da12';
+    expect(functions.isPhoneNumber(phoneNumber)).toBe(false);
+})
+
+// isEmail() Unit Tests
+test('expects test@example.com to be a valid email', () => {
+    const email = 'test@example.com';
+    expect(functions.isEmail(email)).toBe(true);
+})
+
+test('expects hellothere23@school.edu to be a valid email', () => {
+    const email = 'hellothere23@school.edu';
+    expect(functions.isEmail(email)).toBe(true);
+})
+
+test('expects ma@a-die.edu.edu to be an invalid email', () => {
+    const email = 'ma@a-die.edu.edu';
+    expect(functions.isEmail(email)).toBe(false);
+})
+
+test('expects hello.edu to be an invalid email', () => {
+    const email = 'hello.edu';
+    expect(functions.isEmail(email)).toBe(false);
+})
+
+// isStrongPassword() Unit Tests
+test('expects Hello_1234 to be a strong password', () => {
+    const password = 'Hello_1234';
+    expect(functions.isStrongPassword(password)).toBe(true);
+})
+
+test('expects NoW4y1234H0me to be a strong password', () => {
+    const password = 'NoW4y1234H0me';
+    expect(functions.isStrongPassword(password)).toBe(true);
+})
+
+test('expects hi to not be a strong password', () => {
+    const password = 'hi';
+    expect(functions.isStrongPassword(password)).toBe(false);
+})
+
+test('expects 23hellothere23 to not be a strong password', () => {
+    const password = '23hellothere23';
+    expect(functions.isStrongPassword(password)).toBe(false);
+})
+
+// isDate() Unit Tests
+test('expects 08/23/2009 to be a valid date', () => {
+    const date = '08/23/2009';
+    expect(functions.isDate(date)).toBe(true);
+})
+
+test('expects 09/01/1920 to be a valid date', () => {
+    const date = '09/01/1920';
+    expect(functions.isDate(date)).toBe(true);
+})
+
+test('expects 009/01/1000 to be an invalid date', () => {
+    const date = '009/01/1000';
+    expect(functions.isDate(date)).toBe(false);
+})
+
+test('expects 09292902 to be an invalid date', () => {
+    const date = '09292902';
+    expect(functions.isDate(date)).toBe(false);
+})
+
+// isHexColor() Unit Tests
+test('expects #20DA09 to be a valid hex color', () => {
+    const hexColor = '#20DA09';
+    expect(functions.isHexColor(hexColor)).toBe(true);
+})
+
+test('expects #000900 to be a valid hex color', () => {
+    const hexColor = '#000900';
+    expect(functions.isHexColor(hexColor)).toBe(true);
+})
+
+test('expects 9aa09FF to be an invalid hex color', () => {
+    const hexColor = '9aa09FF';
+    expect(functions.isHexColor(hexColor)).toBe(false);
+})
+
+test('expects #__0fds to be an invalid hex color', () => {
+    const hexColor = '#__0fds';
+    expect(functions.isHexColor(hexColor)).toBe(false);
+})
